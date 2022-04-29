a41797852cc1531ab9083abeb6a34e77a4c84d24
Your Name <olamide2497@gmail.com>
2022-04-29 17:29:42 +0100

    Add: onclick to antonyms and synonyms to fetch meaning

--- src/components/Antonyms.jsx
+++ src/components/Antonyms.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './antonyms.css';
 import { nanoid } from 'nanoid';
+import { SearchContext } from '../App';
 
 const Antonyms = ({ meaning }) => {
+
+  const { setSearchValue} = useContext(SearchContext);
+
+  const handleClick = (e) => {
+    setSearchValue(e.target.textContent);
+  }
   return (
     <div><div className="ants">
     {meaning.antonyms.length > 0? <h4>Antonyms</h4>: null}
     {meaning.antonyms.map(ant => 
-        <div key={nanoid()} className="ant">{ant}</div>
+        <div onClick={handleClick} key={nanoid()} className="ant">{ant}</div>
     )}
     </div></div>
   )
--- src/components/Synonyms.jsx
+++ src/components/Synonyms.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { nanoid } from 'nanoid'
 import './synonyms.css';
+import { SearchContext } from '../App';
 
 const Synonyms = ({ meaning }) => {
+
+  const { setSearchValue} = useContext(SearchContext);
+
+  const handleClick = (e) => {
+    setSearchValue(e.target.textContent);
+  }
+
   return (
     <div key={nanoid()} className="syns">
     {meaning.synonyms.length > 0? <h4>Synonyms</h4>: null}
     {meaning.synonyms.map(syn => 
-        <div key={nanoid()} className="syn">{syn}</div>
+        <div onClick={handleClick} key={nanoid()} className="syn">{syn}</div>
     )}
     </div>
   )
